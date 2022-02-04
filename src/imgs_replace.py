4b84f011c793aa237e9ff0d11f4e74b7f81fea26
lipicoder <lipeng@zepan.com.cn>
2022-02-04 11:07:00 +0800

    file

--- src/imgs_replace.py
+++ src/imgs_replace.py
@@ -8,16 +8,26 @@ def replace_img_src(root_dir, dest_dir):
         logging.info("dest directory is null")
         return
 
-    # 创建根目录，如果存在就不做处理
+    # create root directory,if exist do nothing
     os.makedirs(dest_dir, exist_ok=True)
-    logging.info("create dest dir:", dest_dir)
+    logging.info("create dest dir:%s", dest_dir)
 
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        # offset = len(dirpath.split(os.sep))
-        # print("    " * (offset - 1), dirpath, sep="")
-        # print("    " * (offset - 1), dirnames, sep="")
+    for dir_path, _dir_names, filenames in os.walk(root_dir):
 
-        for a_file in filenames:
-            print(dirpath, "/", a_file, sep="")
-            pass
-        pass
+        for file in filenames:
+            # create directory in dest dir
+            dest_dir_new = dir_path.replace(root_dir, dest_dir)
+
+            filename, file_extension = os.path.splitext(file)
+            if file_extension == ".md":
+                print(dir_path, "/", file, sep="")
+                print(dest_dir_new, "/", file, sep="")
+                md_file = dir_path + "/" + file
+                dest_file = dest_dir_new + "/" + file
+                img_src_change(md_file, dest_file)
+
+
+# md_file,dest_file : file with path
+def img_src_change(md_file, dest_file):
+    print("md file, dest file", md_file, dest_file)
+    pass
--- upload.py
+++ upload.py
@@ -4,15 +4,14 @@ import sys
 
 from src import replace_img_src
 
-# logging.basicConfig(filename="example.log", encoding="utf-8", level=logging.DEBUG)
+logging.basicConfig(filename="markdown.log", encoding="utf-8", level=logging.DEBUG)
 
-logging.basicConfig(stream=sys.stdout, encoding="utf-8", level=logging.DEBUG)
 
 if __name__ == "__main__":
+    print("Starting")
     # replace_img_src(
     #     "/Users/dialling/Library/Mobile Documents/com~apple~CloudDocs/md",
     #     "/Users/dialling/Library/Mobile Documents/com~apple~CloudDocs/md_upload",
     # )
 
-    replace_img_src("/Users/dialling/dev/cubox", "markdown_online")
-    print("Starting")
+    replace_img_src("/home/me/Downloads/Archive/md", "markdown_online")
